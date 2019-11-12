6ae30f2016306e9bdd8d270e8fcbdf4d69c118bf
Marina Nakagawa <mari10172001@yahoo.co.jp>
2019-11-12 18:50:06 +0900

    part 11くりあ

--- part-11/object.js
+++ part-11/object.js
@@ -1,20 +1,29 @@
-var startTime = null;
-function start() {
-    startTime = Date.now();
-    //
-    //https://developer.mozilla.org/ja/docs/Web/API/GlobalEventHandlers/onkeypress
-    document.body.onkeypress = stop;
-    console.log("スタートしました");
-}
-
-function stop() {
-    console.log("ストップしました")
+var game = {
+    startTime: null,
+    displayArea: document.getElementById("display-area"),
+    start: function() {
+        game.startTime = Date.now();
+        //
+        //https://developer.mozilla.org/ja/docs/Web/API/GlobalEventHandlers/onkeypress
+        document.body.onkeypress = game.stop;
+        console.log("スタートしました");
+    },
+    stop: function() {
+        var currentTime = Date.now();
+        var seconds = (currentTime - game.startTime) / 1000;
+        if (9.5 <= seconds && seconds <= 10.5){
+            game.displayArea.innerText = seconds;
+        } else {
+            game.displayArea.innerText = seconds;
+        }
+        console.log("ストップしました")
+    }
 }
 
 //memo
 //引数で渡した文字列をダイアログで表示する関数でアラートと異なり、
 //trueとfalseが返り値に帰ってくる
 if (confirm("OKを押して10秒だとおもったらなにかキーを押す")) {
-    start()
+    game.start()
 }
 
