d8b7fbeb1f4e2dc4a68d66a24c82d06d4531ab4d
Marina Nakagawa <mari10172001@yahoo.co.jp>
2019-11-15 14:38:51 +0900

    準備仕様まで

---
+++ Preparation/part-02-10/niconico-ranking.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+dirname="/Users/nakagawa/Project/2019-Nyobi/part-02-10/niconico-ranking-rss"
+mkdir -p ${dirname}
+filename="${dirname}/hourly-ranking-`date +'%Y%m%d%H%M'`.xml"
+echo "Save to $filename"
+curl -s -o $filename -H "User-Agent: CrawlBot; your@mail" https://www.nicovideo.jp/ranking/genre/all?rss=2.0&lang=ja-jp
